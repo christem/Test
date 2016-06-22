26a1b6ac2d45eb09a02f442b55b0122cea1562fd
christem <493420709@qq.com>
2016-06-23 06:43:26 +0800

    netty

--- src/main/java/com/phei/netty/c2/bio/TimeServer.java
+++ src/main/java/com/phei/netty/c2/bio/TimeServer.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.phei.netty.bio;
+package com.phei.netty.c2.bio;
 
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -26,36 +26,36 @@ import java.net.Socket;
  */
 public class TimeServer {
 
-    /**
-     * @param args
-     * @throws IOException
-     */
-    public static void main(String[] args) throws IOException {
-	int port = 8080;
-	if (args != null && args.length > 0) {
+	/**
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+		int port = 8080;
+		if (args != null && args.length > 0) {
 
-	    try {
-		port = Integer.valueOf(args[0]);
-	    } catch (NumberFormatException e) {
-		// 采用默认值
-	    }
+			try {
+				port = Integer.valueOf(args[0]);
+			} catch (NumberFormatException e) {
+				// 采用默认值
+			}
 
+		}
+		ServerSocket server = null;
+		try {
+			server = new ServerSocket(port);
+			System.out.println("The time server is start in port : " + port);
+			Socket socket = null;
+			while (true) {
+				socket = server.accept();
+				new Thread(new TimeServerHandler(socket)).start();
+			}
+		} finally {
+			if (server != null) {
+				System.out.println("The time server close");
+				server.close();
+				server = null;
+			}
+		}
 	}
-	ServerSocket server = null;
-	try {
-	    server = new ServerSocket(port);
-	    System.out.println("The time server is start in port : " + port);
-	    Socket socket = null;
-	    while (true) {
-		socket = server.accept();
-		new Thread(new TimeServerHandler(socket)).start();
-	    }
-	} finally {
-	    if (server != null) {
-		System.out.println("The time server close");
-		server.close();
-		server = null;
-	    }
-	}
-    }
 }
