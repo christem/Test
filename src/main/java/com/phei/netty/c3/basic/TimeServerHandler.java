8357ba7116993a177ded4aaa273f6ce2bd3efd29
christem <493420709@qq.com>
2016-12-12 22:30:16 +0800

    netty c3

--- src/main/java/com/phei/netty/c3/basic/TimeServerHandler.java
+++ src/main/java/com/phei/netty/c3/basic/TimeServerHandler.java
@@ -13,41 +13,19 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-package com.phei.netty.basic;
+package com.phei.netty.c3.basic;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
 
-import java.util.logging.Logger;
-
 /**
  * @author lilinfeng
  * @date 2014年2月14日
  * @version 1.0
  */
-public class TimeClientHandler extends ChannelHandlerAdapter {
-
-    private static final Logger logger = Logger
-	    .getLogger(TimeClientHandler.class.getName());
-
-    private final ByteBuf firstMessage;
-
-    /**
-     * Creates a client-side handler.
-     */
-    public TimeClientHandler() {
-	byte[] req = "QUERY TIME ORDER".getBytes();
-	firstMessage = Unpooled.buffer(req.length);
-	firstMessage.writeBytes(req);
-
-    }
-
-    @Override
-    public void channelActive(ChannelHandlerContext ctx) {
-	ctx.writeAndFlush(firstMessage);
-    }
+public class TimeServerHandler extends ChannelHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg)
@@ -56,14 +34,20 @@ public class TimeClientHandler extends ChannelHandlerAdapter {
 	byte[] req = new byte[buf.readableBytes()];
 	buf.readBytes(req);
 	String body = new String(req, "UTF-8");
-	System.out.println("Now is : " + body);
+	System.out.println("The time server receive order : " + body);
+	String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new java.util.Date(
+		System.currentTimeMillis()).toString() : "BAD ORDER";
+	ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+	ctx.write(resp);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+	ctx.flush();
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-	// 释放资源
-	logger.warning("Unexpected exception from downstream : "
-		+ cause.getMessage());
 	ctx.close();
     }
 }
